05dff750019568df392b64b4287575d916c46ca5
IVIuho <eogh2811@naver.com>
2023-08-07 17:42:06 +0900

    ✨ Add rds connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+import { AWS } from '@config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from 'app.service';
+import { User } from 'entity/user';
+import { UserModule } from 'user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: AWS.RDS.host,
+      port: AWS.RDS.port,
+      username: AWS.RDS.username,
+      password: AWS.RDS.password,
+      database: AWS.RDS.database,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
