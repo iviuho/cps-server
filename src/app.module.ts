8568b1a5be979217825b15a399bf82c148706ee9
IVIuho <eogh2811@naver.com>
2023-08-11 18:43:20 +0900

    🔨 Add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppAccessToken } from './entity/token';
-import { User } from './entity/user';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TokenModule } from './token/token.module';
-import { UserModule } from './user/user.module';
+import { DatabaseModule } from '@src/database/database.module';
+import { TokenModule } from '@src/token/token.module';
+import { UserModule } from '@src/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('RDS_HOST'),
-        port: configService.get<number>('RDS_PORT'),
-        username: configService.get<string>('RDS_USERNAME'),
-        password: configService.get<string>('RDS_PASSWORD'),
-        database: configService.get<string>('RDS_DATABASE'),
-        entities: [AppAccessToken, User],
-        synchronize: true,
-      }),
-    }),
+    DatabaseModule,
     TokenModule,
     UserModule,
   ],
--- src/token/token.service.spec.ts
+++ src/token/token.service.spec.ts
@@ -1,11 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TokenService } from './token.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppAccessToken } from '@src/entity/token';
+
+import { DatabaseModule } from '@src/database/database.module';
+import { TokenService } from '@src/token/token.service';
 
 describe('TokenService', () => {
   let service: TokenService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        DatabaseModule,
+        TypeOrmModule.forFeature([AppAccessToken]),
+        HttpModule,
+      ],
       providers: [TokenService],
     }).compile();
 
