9fe3497923ae79c84bbba9ec8652cd4a11dc28db
IVIuho <eogh2811@naver.com>
2023-08-13 19:46:20 +0900

    ✨ Add comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentModule } from '@src/comment/comment.module';
 import { DatabaseModule } from '@src/database/database.module';
 import { UserModule } from '@src/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.dev.env',
+    }),
     DatabaseModule,
+    CommentModule,
     UserModule,
   ],
   controllers: [AppController],
