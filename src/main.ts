6e2c1e8973300b29cd57143e482d340020337f55
iviuho <iviuho@gmail.com>
2023-08-13 19:44:54 +0900

    ⚡ Add http request logger(morgan)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 
 import { AppModule } from '@src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['debug'],
-  });
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    morgan(
+      ':remote-addr :remote-user [:date[iso]] ":method :url" :status :response-time ms'
+    )
+  );
 
   await app.listen(3000);
 }
