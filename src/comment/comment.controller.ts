319b7fd932e9e5d5bae772805492fac35a1f7c59
IVIuho <eogh2811@naver.com>
2023-08-14 19:53:58 +0900

    ✨ Add comment api

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+
+import { CommentService } from './comment.service';
+import { CommentQuery, Pagination } from './comment.interface';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Get()
+  async getCommentsByQuery(@Query() query: any) {
+    const { to, from }: CommentQuery = query;
+    const { offset, limit }: Pagination = query;
+
+    return await this.commentService.getCommentsByUser(
+      { to, from },
+      { offset, limit }
+    );
+  }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Comment } from '@src/entity/comment';
+
+import { CommentQuery, Pagination } from './comment.interface';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>
+  ) {}
+
+  async getCommentsByUser(query: CommentQuery, pagination: Pagination) {
+    if (!query.to && !query.from) {
+      throw Error('get comments query not found');
+    }
+
+    const { offset = 0, limit = 20 } = pagination;
+
+    return this.commentRepository.find({
+      order: { createdAt: 'DESC' },
+      relations: { from: true, to: true },
+      skip: offset * limit,
+      take: limit,
+      where: { to: { login: query.to }, from: { login: query.from } },
+    });
+  }
+}
