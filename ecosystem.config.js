b8340261433aa4c32137f54c6631f5debf6ad8fd
iviuho <iviuho@gmail.com>
2023-08-26 16:40:56 +0900

    🔧 Fix pm2 run script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: 'yarn run start',
+      script: 'dist/main.js',
       cwd: '/home/bitnami/cps-server',
       env_production: { NODE_ENV: 'production' },
       env_development: { NODE_ENV: 'development' },
